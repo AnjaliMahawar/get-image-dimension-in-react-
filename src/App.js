03c098166285729cb87a47e8af9e6a58de300447
AnjaliMahawar <anjalimahawar011@gmail.com>
2023-01-04 16:35:02 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const loadImage = (setImageDimensions, imageUrl) => {
+  const img = new Image();
+  img.src = imageUrl;
+
+  img.onload = () => {
+    setImageDimensions({
+      height: img.height,
+      width: img.width,
+    });
+  };
+  img.onerror = (err) => {
+    console.log("img error");
+    console.error(err);
+  };
+};
+
+const App = () => {
+  const [imageDimensions, setImageDimensions] = useState({});
+  const [imageUrl, setimageUrl] = useState("");
+  // const imageUrl = "https://picsum.photos/200/300";
+
+  // useEffect(() => {
+
+  // }, []);
+  const getDia = () => {
+    loadImage(setImageDimensions, imageUrl);
+    console.log(imageDimensions);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <>
+        <input
+          type="text"
+          value={imageUrl}
+          onChange={(e) => {
+            setimageUrl(e.target.value);
+          }}
+        />
+        <button onClick={getDia}>getImageDimension</button>
+        {Object.keys(imageDimensions).length === 0 ? (
+          ""
+        ) : (
+          <>
+            <p>
+              <b>Height:</b> {imageDimensions.height}{" "}
+            </p>
+            <p>
+              <b>Width:</b> {imageDimensions.width}{" "}
+            </p>
+          </>
+        )}
+      </>
     </div>
   );
-}
+};
 
 export default App;
